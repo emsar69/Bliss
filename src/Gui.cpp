e8f45dd39fb59714902122ff7130712f24760c97
emsar69 <72223726+emsar69@users.noreply.github.com>
2026-08-09 12:54:56 +0300

    Read timer directly if It is local player.
    Co-authored-by: Linus Torvalds <torvalds@linux-foundation.org>

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -230,8 +230,7 @@ void HandleFeatures(){
 
 				if(isBad) {
 					// Draw kill cooldown
-					//float timer = player.self == Game::g_LocalPlayer.self ? *player.GetKillTimer() : player.GetKillTimerState();
-					float timer = player.GetKillTimerState();
+					float timer = player.self == Game::g_LocalPlayer.self ? *player.GetKillTimer() : player.GetKillTimerState();
 					int G = 50+timer*10;
 					if(G > 255) G = 255;
 
