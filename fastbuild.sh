ba4095230f34c0c032e6b6609bd93d40bb1a2fd8
emsar69 <72223726+emsar69@users.noreply.github.com>
2026-08-29 22:15:21 +0300

    Build both ABI (or more by editing script)

--- fastbuild.sh
+++ fastbuild.sh
@@ -8,17 +8,22 @@ C=/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-andro
 CXX=/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang++
 APK=build/apk
 KEY_PASS=123456
-ABI=arm64-v8a # 64bit=arm64-v8a | 32bit=armeabi-v7a
+BASE=base
 
-cmake -B build -G Ninja -DCMAKE_C_COMPILER=${C} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_ANDROID_NDK=/opt/android-ndk -DANDROID_ABI=${ABI}
+for ABI in arm64-v8a armeabi-v7a; do
+    echo Building: $ABI
+    mkdir -p ${BASE}/lib/${ABI}
 
-ninja -C build
+    rm -rf build
+    cmake -B build -G Ninja -DCMAKE_C_COMPILER=${C} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_ANDROID_NDK=/opt/android-ndk -DANDROID_ABI=${ABI}
+    ninja -C build
+    cp build/libBliss.so ${BASE}/lib/${ABI}/libbliss.so
+done
 
-cp build/libBliss.so base/lib/arm64-v8a/libbliss.so
 mkdir -p ${APK}
 rm -rf ${APK}/*
 
-apktool b base -o ${APK}/amongus.apk
+apktool b ${BASE} -o ${APK}/amongus.apk
 uber-apk-signer --apks ${APK}/amongus.apk --ks key.jks --ksAlias key --ksPass ${KEY_PASS} --ksKeyPass ${KEY_PASS} -o ${APK}
 rm ${APK}/amongus.apk ${APK}/amongus-aligned-signed.apk.idsig
 
